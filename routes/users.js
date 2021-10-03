897f72b21c24f824a3cb293fb89ca5a0dd7295af
rajel89 <rajah.ello@yahoo.com>
2021-10-03 12:27:55 -0500

    created user registration router in users.js '(router.post('/register)'.

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,14 @@ const bcrypt = require('bcrypt');
 const auth = require('../middleware/auth');
 const express = require('express');
 const admin = require('../middleware/admin');
+const res = require('express/lib/response');
+const req = require('express/lib/request');
 const router = express.Router();
 
 
 //functions to be able to GET, POST, PUT user//
 
 //This is finding user by ID, the users ID will cary the services offered listed as a array of services.
-
-
 //Admin only
 router.get('/', async (req, res) => {
     try{
@@ -21,7 +21,6 @@ router.get('/', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
-
 
 router.get('/:id', async (req, res) => {
     try{
@@ -33,7 +32,6 @@ router.get('/:id', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
-
 
 //This is finding a user's POST
 router.get('/:userId/post', async (req, res) => {
@@ -48,6 +46,7 @@ router.get('/:userId/post', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
 //This is to create a USER with authentication requirement using jwtToken//
 router.post('/', auth, async (req, res) => {
     try{
@@ -77,7 +76,6 @@ router.post('/', auth, async (req, res) => {
     }
 });
 
-
 router.post('/:userId/posts', async (req, res) => {
     try{
         const user = await User.findById(req.params.userId);
@@ -96,6 +94,7 @@ router.post('/:userId/posts', async (req, res) => {
         return res.status(500).send(`Internal Server Error: ${ex}`);
     }
 });
+
 //Update user information e-mail, name, password
 router.put('/:id', async (req, res) => {
     try{
@@ -136,10 +135,6 @@ router.delete('/:userId', auth, async (req, res) => {
 });
 
 
-
-
-
-
 //This is to delete a services posted
 router.delete('/:userId/post/postId', auth, async (req, res) => {
     try {
@@ -155,8 +150,41 @@ router.delete('/:userId/post/postId', auth, async (req, res) => {
     }
 });
 
+router.post('/register', sync(req, res) => {
+    //return res.send(req.body);
+    try{let validation = validateUser(req.body);
+        if (validation?.error)
+        {
+            res.status(400).send(validation.error.details[0].message); 
+        }
+        let user = await User.frindOne({email: req.body.email});
+        if (user) return res.status(400).send(`Email ${req.body.mail} is already registered.`);
+
+        const salt = await bcrypt.genSalt(10);
+        user = new user({
+            name: req.body.name,
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, salt),
+            accountType: req.body.accountType
+        });
+        await user.save();
+
+    if(req.body.accountType == "seller")
+    {
+        let msg = "You're all set. Now you can create service packages and start generating revenue.";
+        return res.status(200).send({"success": true, "data": {"name": req.body.name, "email": req.body.emai}, "msg": msg})    
+    }else if(req.body.accountType == "buyer")
+    {
+        let msg =" You're all set, Now you can start to hire freelancers to grow your business.";
+        return res.status(200).send({"success": true, "data": {"name": req.body.name, "email": req.body.emai}, "msg": msg})
+    }
+
+}catch(e){
+    return res.status(500).send(`Interanl Server Error: ${e}`);
+}
+});
 
 
 
 
-module.exports= router;
+module.exports= router;s
